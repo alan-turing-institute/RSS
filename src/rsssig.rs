812cdb092f7bf9cc6e0506aa8413724df88815a7
Bryan <70968819+brykumara@users.noreply.github.com>
2023-05-10 15:15:11 +0100

    updated index for redacted signature

--- src/rsssig.rs
+++ src/rsssig.rs
@@ -124,24 +124,22 @@ impl RSignature{
     pub fn rss_derive_signature(pk:PKrss, rsig: RSignature,messages: &[FieldElement],index: Vec<usize>) -> (RSignature, RedactedMessage){
         let r = FieldElement::random(); // Generate r
         let t = FieldElement::random(); // Generate t
-        let r_clone = FieldElement::clone(&r); // Clone it for repeated uses
-        let t_clone = FieldElement::clone(&t); // Clone it for repeated uses 
         let sigma_1_prime = rsig.sigma_1.scalar_mul_const_time(&r); // sigma'1 = sigma1 * r 
-        let sigma_2_r = rsig.sigma_2.scalar_mul_const_time(&r_clone); // sigma2 mul r 
-        let sigma_1_prime_t = sigma_1_prime.scalar_mul_const_time(&t_clone); // sigma'1 mul t
+        let sigma_2_r = rsig.sigma_2.scalar_mul_const_time(&r); // sigma2 mul r 
+        let sigma_1_prime_t = sigma_1_prime.scalar_mul_const_time(&t); // sigma'1 mul t
         let sigma_2_prime = sigma_1_prime_t.add(sigma_2_r) ; // sigma'2 = (sigma2 mul r) + (sigma'1 mul t)
         
-        let mut accumulator = pk.X_tilde.scalar_mul_const_time(&FieldElement::new()); // set to 0 for sum of Y~j mul mj
+        let mut accumulator = VerkeyGroup::new(); // set to 0 for sum of Y~j mul mj
         for j in 0..messages.len(){
             if !index.contains(&j){
                 let Y_tilde_i = &pk.Y_tilde_i[j]; // select Y~[j]
                 let Y_tilde_i_mj = Y_tilde_i.scalar_mul_const_time(&messages[j]); // Y~[j] mul m[j]
-                accumulator = accumulator.add(Y_tilde_i_mj); // Sum Y~[i] mul m[j]
+                accumulator += Y_tilde_i_mj; // Sum Y~[i] mul m[j]
             }
         };
 
-        let g_tilde_t = pk.g_tilde.scalar_mul_const_time(&t_clone) ; // g~ mul t
-        let sigma_prime_tilde = g_tilde_t.add(accumulator); // sigma~' = g~ mul t + sum of (Y~[j] mul m[j])
+        let g_tilde_t = pk.g_tilde.scalar_mul_const_time(&t) ; // g~ mul t
+        let sigma_prime_tilde = g_tilde_t + accumulator; // sigma~' = g~ mul t + sum of (Y~[j] mul m[j])
 
         let sigma_1_prime_string = sigma_1_prime.to_string(); // convert sigma1' to string
         let sigma_2_prime_string = sigma_2_prime.to_string(); // convert sigma2' to string
@@ -164,39 +162,32 @@ impl RSignature{
                 c[i]=(FieldElement::new());
             }
         }
-        let mut in_I= 0; // counter to go through all of index I
-        let mut not_in_I=0; // counter to go through all of j not in I
-        let mut z= FieldElement::new(); // counter to refer parts of a message m_j
         let mut sigma_3_prime = SignatureGroup::new(); // create an empty element for sigma_3
-
-        let mut i: i32= 0; // create a counter to traverse message length
+        //println!("{:?}", clone_index);
         // NOTE: i is only for 1 to n, but we've been working with 0<=i<n. I changed Y^t_n+1-i to Y^t_n-i to account for this.
-        for _ in 0..messages.len(){
-            if clone_index.contains(&in_I){ // Checks if p is in index
-                let mut Y_mj = SignatureGroup::new(); // create blank to store Y^mj
-                if clone_index.contains(&not_in_I){
-                    not_in_I += 1;
-
-                } else { // identify elements not in index
-                    let mut Y_mji = SignatureGroup::new();
-                    let Y_mj_index = messages.len() - in_I + not_in_I ; // define index for Y^mj
-                    if Y_mj_index > messages.len(){
-                        Y_mji = pk.Y_k_nplus2_to_2n[Y_mj_index].clone();
-                    } else {
-                        Y_mji = pk.Y_j_1_to_n[Y_mj_index].clone();
-                    }
-                     // create blank to find each constituent of Y^mj
-                    Y_mj = Y_mj.add(Y_mji); // acumulate for Y_mj
-                    not_in_I += 1; // increment index
-                }
-                //let mut Y_t = SignatureGroup::new(); // create an empty group element to store Y^t
-                let mut Y_t_i = SignatureGroup::new(); // create blank to find each constituent of Y^t
-                Y_t_i = pk.Y_j_1_to_n[messages.len()-in_I-1].clone().scalar_mul_const_time(&t) + Y_mj;
-                sigma_3_prime = sigma_3_prime.add(Y_t_i.scalar_mul_const_time(&c[in_I]));
-                in_I += 1;
-            } else {
-                in_I +=1;
+        let vec_in_I = clone_index.clone();
+        let mut vec_not_in_I: Vec<usize> = Vec::new();
+        for j in 0..messages.len(){
+            if !clone_index.contains(&j){
+            vec_not_in_I.push(j);
             }
+        }      
+        for i in vec_in_I{
+            let mut Y_mj = SignatureGroup::new(); // create blank to store Y^mj
+            for j in &vec_not_in_I{
+                let mut Y_mji = SignatureGroup::new();
+                let Y_mj_index = messages.len() - i + j;
+                println!("{:?}", Y_mj_index);
+                if Y_mj_index > messages.len(){
+                    Y_mji = pk.Y_k_nplus2_to_2n[Y_mj_index-messages.len()-1].clone();
+                } else {
+                    Y_mji = pk.Y_j_1_to_n[Y_mj_index].clone();
+                }
+                Y_mj +=Y_mji; // acumulate for Y_mj
+            }
+            let mut Y_t_i = SignatureGroup::new(); // create blank to find each constituent of Y^t
+            Y_t_i = pk.Y_j_1_to_n[messages.len()-i-1].clone().scalar_mul_const_time(&t) + Y_mj;
+            sigma_3_prime = Y_t_i.scalar_mul_const_time(&c[i]);
         }
         let redacted_message = messages.to_redacted_message(clone_index);
         (RSignature{sigma_1: (sigma_1_prime), sigma_2: (sigma_2_prime), sigma_3: (sigma_3_prime), sigma_4:(sigma_prime_tilde)},  redacted_message)
@@ -318,7 +309,7 @@ impl RSignature{
                 }
             }
             let fourth_equation = GT::ate_pairing(&rsig.sigma_4, &accumulator_2);
-            println!("{:?}",fourth_equation);
+            //println!("{:?}",fourth_equation);
             if third_equation == fourth_equation{
                 test_check = true;
                 println!("Passed second test");
